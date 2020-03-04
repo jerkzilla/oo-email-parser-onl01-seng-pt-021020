efe2ce7cb0abf157606cd0c6e1094e7c6a54bf2b
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-04 14:30:01 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,9 +3,11 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  attr_accessor
+  attr_accessor :name :csv_emails
 
-  def initialize
+  def initialize (csv_emails)
+
+    @csv_emails = csv_emails
 
 
   end
